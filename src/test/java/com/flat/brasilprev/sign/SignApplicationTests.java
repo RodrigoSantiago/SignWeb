edbe71d54a042a4e9f32296e53c949343db73d67
RodrigoSantiago <r0dr160_tj@hotmail.com>
2021-08-11 22:43:43 -0300

    Modelo de automação de testes para JPA

--- src/test/java/com/flat/brasilprev/sign/SignApplicationTests.java
+++ src/test/java/com/flat/brasilprev/sign/SignApplicationTests.java
@@ -1,13 +1,31 @@
 package com.flat.brasilprev.sign;
 
+import com.flat.brasilprev.sign.controller.CepController;
+import com.flat.brasilprev.sign.controller.IndexController;
+import com.flat.brasilprev.sign.controller.LoginController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
 
 @SpringBootTest
 class SignApplicationTests {
 
+	@Autowired
+	private IndexController indexController;
+
+	@Autowired
+	private LoginController loginController;
+
+	@Autowired
+	private CepController cepController;
+
 	@Test
 	void contextLoads() {
+		assertThat(indexController).isNotNull();
+		assertThat(loginController).isNotNull();
+		assertThat(cepController).isNotNull();
 	}
 
 }
--- src/test/java/com/flat/brasilprev/sign/UserTest.java
+++ src/test/java/com/flat/brasilprev/sign/UserTest.java
@@ -1,7 +1,37 @@
 package com.flat.brasilprev.sign;
 
+import com.flat.brasilprev.sign.model.User;
+import com.flat.brasilprev.sign.model.UserRepository;
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.util.List;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class UserTest {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    void userSave() {
+        User user = new User();
+        user.setNome("Rodrigo");
+        user.setSenha("1234");
+        user.setCpf("123.456.789-10");
+        user.setCep("64066-000");
+        user.setCidade("Cidade");
+        user.setBairro("Bairro");
+        user.setUf("UF");
+        userRepository.save(user);
+
+        List<User> users = userRepository.findByNome("Rodrigo");
+        assertThat(users.size()).as("Usuário não encontrado no banco").isEqualTo(1);
+        assertThat(users.get(0).getNome()).as("Usuário incorreto").isEqualTo("Rodrigo");
+    }
 }
